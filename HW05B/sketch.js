11b991f8c1100acd8a02cc28cd0d0914b14e6d6c
Lauren Tsao <et2676@nyu.edu>
2024-10-10 12:29:25 -0400

    cleaned up code

--- HW05B/sketch.js
+++ HW05B/sketch.js
@@ -1,21 +1,18 @@
 function preload() {
-  myData = loadStrings('/assets/cloudnames.txt');
+  myData = loadStrings("/assets/cloudnames.txt");
 }
 
 function cloud(x, y, size) {
-  
   let betweenX = mouseX > x - size && mouseX < x + size;
   let betweenY = mouseY > y - size && mouseY < y + size;
 
- 
-  noStroke()
+  noStroke();
   ellipse(x, y, size);
   ellipse(x + 25, y + 20, size);
   ellipse(x - 20, y + 20, size);
   ellipse(x - 40, y + 5, size);
   ellipse(x - 65, y + 25, size);
 
-  
   let phrase = random(myData);
   if (betweenX && betweenY) {
     text(phrase, x, y);
@@ -28,12 +25,12 @@ function setup() {
 
 function draw() {
   m = millis();
-  background('cornflowerBlue');
+  background("cornflowerBlue");
 
   if (m > 1000) {
-    let x1pos = 0.9 * frameCount / 2;
+    let x1pos = (0.9 * frameCount) / 2;
     cloud(x1pos % width, height / 1.2, 60);
-}
+  }
 
   if (m > 3000) {
     let x2pos = 0.8 * frameCount;
@@ -41,7 +38,7 @@ function draw() {
   }
 
   if (m > 5000) {
-    let x3pos = 0.9 * frameCount / 2;
+    let x3pos = (0.9 * frameCount) / 2;
     cloud(x3pos % width, height / 12, 80);
   }
 
@@ -55,13 +52,13 @@ function draw() {
     cloud(x5pos % width, height / 5, 70);
   }
 
-    if (m > 11000) {
-      let x6pos = 1.2 * frameCount;
-      cloud(x6pos % width, height / 5, 80);
-    }
+  if (m > 11000) {
+    let x6pos = 1.2 * frameCount;
+    cloud(x6pos % width, height / 5, 80);
+  }
 
   if (m > 14000) {
-    let x7pos = 5 * frameCount / 2;
+    let x7pos = (5 * frameCount) / 2;
     cloud(x7pos % width, height / 1.6, 60);
   }
 }
